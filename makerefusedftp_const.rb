00cd01ae6925b3f225089072341bcfabd83446ee
ke-i <horykai@gmail.com>
2013-02-10 19:21:43 +0900

    2013/02/10 update
    include const module

---
+++ makerefusedftp_const.rb
@@ -0,0 +1,19 @@
+module MakerefusedftpConst
+
+TITLE = 'FTP不正アクセス一覧'
+#ローカルIP
+SELFLAN = '192.168.24'
+#集計の表示件数
+SUMTOP = 20
+#履歴の表示件数
+HISTORYTOP = 50
+#コマンドラインからのログ名
+LOGNAME = ARGV[0]
+#出力ファイル名
+OUTHTML = 'refusedftp_ng.html'
+#テーブルの色
+TBLBKCOL = '#EEEE00'
+
+VER = "1.0"
+end
+
